93f3f51154829418e8784390a4861c598f2d20c3
Aisarporn <aurasumit1122@gmail.com>
2018-12-11 10:54:31 +0700

    11/12/61

--- WindowsApp4/My Project/Application.Designer.vb
+++ WindowsApp4/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApp4.frm2
+            Me.MainForm = Global.WindowsApp4.frmLab3
         End Sub
     End Class
 End Namespace
