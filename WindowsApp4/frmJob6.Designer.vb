5c425688adb614acf8065cc4c888abaef1993f66
Aisarporn <aurasumit1122@gmail.com>
2018-12-25 12:23:20 +0700

    25/12/61

---
+++ WindowsApp4/frmJob6.Designer.vb
@@ -0,0 +1,374 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmJob6
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.chkCarForMe = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblItemsPrice4 = New System.Windows.Forms.Label()
+        Me.lblItemsPrice3 = New System.Windows.Forms.Label()
+        Me.lblItemsPrice2 = New System.Windows.Forms.Label()
+        Me.lblItemsPrice1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.chkItemsAdd2 = New System.Windows.Forms.CheckBox()
+        Me.chkItemsAdd4 = New System.Windows.Forms.CheckBox()
+        Me.chkItemsAdd1 = New System.Windows.Forms.CheckBox()
+        Me.chkItemsAdd3 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblFree3 = New System.Windows.Forms.Label()
+        Me.lblFree2 = New System.Windows.Forms.Label()
+        Me.lblFree1 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.chkFree2 = New System.Windows.Forms.CheckBox()
+        Me.chkFree1 = New System.Windows.Forms.CheckBox()
+        Me.chkFree3 = New System.Windows.Forms.CheckBox()
+        Me.lblStartPrice = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.lblPaidPrice = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(196, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(187, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "โปรแกรมคำนวณขายรถยนต์"
+        '
+        'chkCarForMe
+        '
+        Me.chkCarForMe.AutoSize = True
+        Me.chkCarForMe.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkCarForMe.Location = New System.Drawing.Point(71, 82)
+        Me.chkCarForMe.Name = "chkCarForMe"
+        Me.chkCarForMe.Size = New System.Drawing.Size(128, 22)
+        Me.chkCarForMe.TabIndex = 1
+        Me.chkCarForMe.Text = "ISUSU MU7X"
+        Me.chkCarForMe.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox1.Controls.Add(Me.lblItemsPrice4)
+        Me.GroupBox1.Controls.Add(Me.lblItemsPrice3)
+        Me.GroupBox1.Controls.Add(Me.lblItemsPrice2)
+        Me.GroupBox1.Controls.Add(Me.lblItemsPrice1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.chkItemsAdd2)
+        Me.GroupBox1.Controls.Add(Me.chkItemsAdd4)
+        Me.GroupBox1.Controls.Add(Me.chkItemsAdd1)
+        Me.GroupBox1.Controls.Add(Me.chkItemsAdd3)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 123)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(285, 209)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "อุปกรณ์เสริม"
+        '
+        'lblItemsPrice4
+        '
+        Me.lblItemsPrice4.BackColor = System.Drawing.Color.White
+        Me.lblItemsPrice4.Location = New System.Drawing.Point(188, 166)
+        Me.lblItemsPrice4.Name = "lblItemsPrice4"
+        Me.lblItemsPrice4.Size = New System.Drawing.Size(62, 22)
+        Me.lblItemsPrice4.TabIndex = 2
+        Me.lblItemsPrice4.Text = "30000"
+        Me.lblItemsPrice4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblItemsPrice3
+        '
+        Me.lblItemsPrice3.BackColor = System.Drawing.Color.White
+        Me.lblItemsPrice3.Location = New System.Drawing.Point(188, 132)
+        Me.lblItemsPrice3.Name = "lblItemsPrice3"
+        Me.lblItemsPrice3.Size = New System.Drawing.Size(62, 22)
+        Me.lblItemsPrice3.TabIndex = 2
+        Me.lblItemsPrice3.Text = "20000"
+        Me.lblItemsPrice3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblItemsPrice2
+        '
+        Me.lblItemsPrice2.BackColor = System.Drawing.Color.White
+        Me.lblItemsPrice2.Location = New System.Drawing.Point(186, 99)
+        Me.lblItemsPrice2.Name = "lblItemsPrice2"
+        Me.lblItemsPrice2.Size = New System.Drawing.Size(64, 22)
+        Me.lblItemsPrice2.TabIndex = 2
+        Me.lblItemsPrice2.Text = "50000"
+        Me.lblItemsPrice2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblItemsPrice1
+        '
+        Me.lblItemsPrice1.BackColor = System.Drawing.Color.White
+        Me.lblItemsPrice1.Location = New System.Drawing.Point(186, 65)
+        Me.lblItemsPrice1.Name = "lblItemsPrice1"
+        Me.lblItemsPrice1.Size = New System.Drawing.Size(64, 22)
+        Me.lblItemsPrice1.TabIndex = 2
+        Me.lblItemsPrice1.Text = "40000"
+        Me.lblItemsPrice1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(197, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 18)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "ราคา"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(23, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "รายการ"
+        '
+        'chkItemsAdd2
+        '
+        Me.chkItemsAdd2.AutoSize = True
+        Me.chkItemsAdd2.Location = New System.Drawing.Point(6, 99)
+        Me.chkItemsAdd2.Name = "chkItemsAdd2"
+        Me.chkItemsAdd2.Size = New System.Drawing.Size(100, 22)
+        Me.chkItemsAdd2.TabIndex = 0
+        Me.chkItemsAdd2.Text = "ชุดเสริมข้าง"
+        Me.chkItemsAdd2.UseVisualStyleBackColor = True
+        '
+        'chkItemsAdd4
+        '
+        Me.chkItemsAdd4.AutoSize = True
+        Me.chkItemsAdd4.Location = New System.Drawing.Point(6, 166)
+        Me.chkItemsAdd4.Name = "chkItemsAdd4"
+        Me.chkItemsAdd4.Size = New System.Drawing.Size(73, 22)
+        Me.chkItemsAdd4.TabIndex = 0
+        Me.chkItemsAdd4.Text = "ล้อแม็ค"
+        Me.chkItemsAdd4.UseVisualStyleBackColor = True
+        '
+        'chkItemsAdd1
+        '
+        Me.chkItemsAdd1.AutoSize = True
+        Me.chkItemsAdd1.Location = New System.Drawing.Point(6, 65)
+        Me.chkItemsAdd1.Name = "chkItemsAdd1"
+        Me.chkItemsAdd1.Size = New System.Drawing.Size(92, 22)
+        Me.chkItemsAdd1.TabIndex = 0
+        Me.chkItemsAdd1.Text = "กันชนหน้า"
+        Me.chkItemsAdd1.UseVisualStyleBackColor = True
+        '
+        'chkItemsAdd3
+        '
+        Me.chkItemsAdd3.AutoSize = True
+        Me.chkItemsAdd3.Location = New System.Drawing.Point(6, 132)
+        Me.chkItemsAdd3.Name = "chkItemsAdd3"
+        Me.chkItemsAdd3.Size = New System.Drawing.Size(95, 22)
+        Me.chkItemsAdd3.TabIndex = 0
+        Me.chkItemsAdd3.Text = "แล็คหลังคา"
+        Me.chkItemsAdd3.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox2.Controls.Add(Me.lblFree3)
+        Me.GroupBox2.Controls.Add(Me.lblFree2)
+        Me.GroupBox2.Controls.Add(Me.lblFree1)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.chkFree2)
+        Me.GroupBox2.Controls.Add(Me.chkFree1)
+        Me.GroupBox2.Controls.Add(Me.chkFree3)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(319, 123)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(285, 209)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "สิทธิพิเศษ"
+        '
+        'lblFree3
+        '
+        Me.lblFree3.BackColor = System.Drawing.Color.White
+        Me.lblFree3.Location = New System.Drawing.Point(188, 132)
+        Me.lblFree3.Name = "lblFree3"
+        Me.lblFree3.Size = New System.Drawing.Size(68, 22)
+        Me.lblFree3.TabIndex = 2
+        Me.lblFree3.Text = "35000"
+        Me.lblFree3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFree2
+        '
+        Me.lblFree2.BackColor = System.Drawing.Color.White
+        Me.lblFree2.Location = New System.Drawing.Point(188, 99)
+        Me.lblFree2.Name = "lblFree2"
+        Me.lblFree2.Size = New System.Drawing.Size(68, 22)
+        Me.lblFree2.TabIndex = 2
+        Me.lblFree2.Text = "15000"
+        Me.lblFree2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFree1
+        '
+        Me.lblFree1.BackColor = System.Drawing.Color.White
+        Me.lblFree1.Location = New System.Drawing.Point(186, 65)
+        Me.lblFree1.Name = "lblFree1"
+        Me.lblFree1.Size = New System.Drawing.Size(70, 23)
+        Me.lblFree1.TabIndex = 2
+        Me.lblFree1.Text = "25000"
+        Me.lblFree1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(197, 34)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(39, 18)
+        Me.Label12.TabIndex = 1
+        Me.Label12.Text = "ราคา"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(46, 38)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(56, 18)
+        Me.Label13.TabIndex = 1
+        Me.Label13.Text = "รายการ"
+        '
+        'chkFree2
+        '
+        Me.chkFree2.AutoSize = True
+        Me.chkFree2.Location = New System.Drawing.Point(6, 99)
+        Me.chkFree2.Name = "chkFree2"
+        Me.chkFree2.Size = New System.Drawing.Size(83, 22)
+        Me.chkFree2.TabIndex = 0
+        Me.chkFree2.Text = "ทำ พรบ."
+        Me.chkFree2.UseVisualStyleBackColor = True
+        '
+        'chkFree1
+        '
+        Me.chkFree1.AutoSize = True
+        Me.chkFree1.Location = New System.Drawing.Point(6, 65)
+        Me.chkFree1.Name = "chkFree1"
+        Me.chkFree1.Size = New System.Drawing.Size(137, 22)
+        Me.chkFree1.TabIndex = 0
+        Me.chkFree1.Text = "ประกันภัยชั้นหนึ่ง"
+        Me.chkFree1.UseVisualStyleBackColor = True
+        '
+        'chkFree3
+        '
+        Me.chkFree3.AutoSize = True
+        Me.chkFree3.Location = New System.Drawing.Point(6, 132)
+        Me.chkFree3.Name = "chkFree3"
+        Me.chkFree3.Size = New System.Drawing.Size(96, 22)
+        Me.chkFree3.TabIndex = 0
+        Me.chkFree3.Text = "จดทะเบียน"
+        Me.chkFree3.UseVisualStyleBackColor = True
+        '
+        'lblStartPrice
+        '
+        Me.lblStartPrice.BackColor = System.Drawing.Color.White
+        Me.lblStartPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStartPrice.Location = New System.Drawing.Point(495, 82)
+        Me.lblStartPrice.Name = "lblStartPrice"
+        Me.lblStartPrice.Size = New System.Drawing.Size(80, 23)
+        Me.lblStartPrice.TabIndex = 2
+        Me.lblStartPrice.Text = "1250000"
+        '
+        'Label14
+        '
+        Me.Label14.BackColor = System.Drawing.Color.White
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(322, 376)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(86, 23)
+        Me.Label14.TabIndex = 2
+        Me.Label14.Text = "ราคาสุทธิ"
+        '
+        'lblPaidPrice
+        '
+        Me.lblPaidPrice.BackColor = System.Drawing.Color.White
+        Me.lblPaidPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPaidPrice.Location = New System.Drawing.Point(450, 376)
+        Me.lblPaidPrice.Name = "lblPaidPrice"
+        Me.lblPaidPrice.Size = New System.Drawing.Size(143, 23)
+        Me.lblPaidPrice.TabIndex = 2
+        Me.lblPaidPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'frmJob6
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(674, 431)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblPaidPrice)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.lblStartPrice)
+        Me.Controls.Add(Me.chkCarForMe)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmJob6"
+        Me.Text = "frmJob6"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents chkCarForMe As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblItemsPrice4 As Label
+    Friend WithEvents lblItemsPrice3 As Label
+    Friend WithEvents lblItemsPrice2 As Label
+    Friend WithEvents lblItemsPrice1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents chkItemsAdd2 As CheckBox
+    Friend WithEvents chkItemsAdd4 As CheckBox
+    Friend WithEvents chkItemsAdd1 As CheckBox
+    Friend WithEvents chkItemsAdd3 As CheckBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblFree3 As Label
+    Friend WithEvents lblFree2 As Label
+    Friend WithEvents lblFree1 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents chkFree2 As CheckBox
+    Friend WithEvents chkFree1 As CheckBox
+    Friend WithEvents chkFree3 As CheckBox
+    Friend WithEvents lblStartPrice As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents lblPaidPrice As Label
+End Class
